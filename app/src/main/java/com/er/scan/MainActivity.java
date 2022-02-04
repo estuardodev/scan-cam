1dd5c956297e582b0fd27ed296488130bebec778
Estuardo Ramírez <estuardito_9@hotmail.com>
2022-02-03 18:59:57 -0600

    se han agregado los menus de terminos y condiciones, se ha agregado el boton de compartir app

--- app/src/main/java/com/er/scan/MainActivity.java
+++ app/src/main/java/com/er/scan/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -48,6 +51,38 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.fragments, fragment);
         transaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_web, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Intent i = new Intent(this, Terminos.class);
+                startActivity(i);
+                return true;
+            case R.id.item2:
+                Intent ie = new Intent(this, Politicas.class);
+                startActivity(ie);
+                return true;
+            case R.id.item3:
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("Text/plain");
+                intent.putExtra(Intent.EXTRA_SUBJECT,getResources().getString(R.string.app_name));
+                String aux = "Hey,descarga esta increíble aplicación.\n¡Descargala y dale 5 estrellas!\n";
+                aux = aux +"https://play.google.com/store/apps/details?id=com.er.scan";
+                intent.putExtra(intent.EXTRA_TEXT, aux);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
