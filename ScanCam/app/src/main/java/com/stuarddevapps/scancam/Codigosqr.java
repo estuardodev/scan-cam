68d9d38b482c0c718fd901f17f343a34627a3dad
Estuardo Ramirez <estuardito_9@hotmail.com>
2022-06-10 00:25:33 -0600

    se configuro el codigo qr

--- ScanCam/app/src/main/java/com/stuarddevapps/scancam/Codigosqr.java
+++ ScanCam/app/src/main/java/com/stuarddevapps/scancam/Codigosqr.java
@@ -1,14 +1,29 @@
 package com.stuarddevapps.scancam;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class Codigosqr extends Fragment {
+    //Variables
+    TextView link;
+    Button btnLeer;
+
 
     public Codigosqr() {
         // Required empty public constructor
@@ -27,6 +42,60 @@ public class Codigosqr extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_codigosqr, container, false);
+        View vista = inflater.inflate(R.layout.fragment_codigosqr, container, false);
+
+        link = vista.findViewById(R.id.link);
+        btnLeer = vista.findViewById(R.id.btnScanQR);
+
+        btnScan();
+
+        return vista;
     }
+
+    private void btnScan(){
+        btnLeer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                escanear();
+            }
+        });
+    }
+
+    public void escanear(){
+        IntentIntegrator intent = IntentIntegrator.forSupportFragment(Codigosqr.this);
+        intent.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+        intent.setPrompt("ESCANEAR CÓDIGO QR");
+        intent.setCameraId(0);
+        intent.setBeepEnabled(true);
+        intent.setOrientationLocked(false);
+        intent.setCaptureActivity(CaptureActivityPortrait.class);
+        intent.setBarcodeImageEnabled(false);
+        intent.initiateScan();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode, data);
+        if (result != null)
+        {
+            if (result.getContents() ==  null)
+            {
+                Toast.makeText(getContext(), "Cancelaste el escaneo", Toast.LENGTH_LONG).show();
+            }else{
+                link.setText(result.getContents().toString());
+                Toast.makeText(getContext(), "Texto copiado al portapapeles", Toast.LENGTH_LONG).show();
+                ClipboardManager clipboard = (ClipboardManager)
+                        getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("simple text", link.getText());
+                clipboard.setPrimaryClip(clip);
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
+
+
+
+        }
+    }
+
+
 }
