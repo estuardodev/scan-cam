1a8acabe981a8bf579f40232b55337de633f2674
Estuardo Ramirez <estuardito_9@hotmail.com>
2022-06-09 23:16:09 -0600

    Se elimino codigo antiguo de main y se creo el nuevo menu en activity main

--- ScanCam/app/src/main/java/com/stuarddevapps/scancam/MainActivity.java
+++ ScanCam/app/src/main/java/com/stuarddevapps/scancam/MainActivity.java
@@ -1,41 +1,10 @@
 package com.stuarddevapps.scancam;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.Toast;
-
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.play.core.review.ReviewInfo;
-import com.google.android.play.core.review.ReviewManager;
-import com.google.android.play.core.review.ReviewManagerFactory;
-import com.google.android.play.core.tasks.OnCompleteListener;
-import com.google.android.play.core.tasks.OnSuccessListener;
-import com.google.android.play.core.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
-    BottomNavigationView bnv;
-    Codigoqr codigoqr = new Codigoqr();
-    CodigoBarras codigoBarras = new CodigoBarras();
-    CodigoOtros codigoOtros = new CodigoOtros();
-
-    ReviewManager manager;
-    ReviewInfo reviewInfo;
-    private AdView mAdView;
-    private AdView mAdView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
