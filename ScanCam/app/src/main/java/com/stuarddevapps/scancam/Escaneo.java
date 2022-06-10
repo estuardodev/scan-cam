2c0c94f0f20f729642bc0402bce54b974d1bf3f9
Estuardo Ramirez <estuardito_9@hotmail.com>
2022-06-10 11:13:27 -0600

    Se agrego la actividad de escaneo

---
+++ ScanCam/app/src/main/java/com/stuarddevapps/scancam/Escaneo.java
@@ -0,0 +1,14 @@
+package com.stuarddevapps.scancam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Escaneo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escaneo);
+    }
+}
